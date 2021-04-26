bebfaa728ebcc15ed49a9872b42277926963d565
Andre <apepbaby147@gmail.com>
2021-04-27 01:25:55 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -13,9 +13,8 @@ $(document).ready(function(){
                     arrows: false
                 }
             }
-        ]
-    });
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        ]    });
+            $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
@@ -96,15 +95,13 @@ $(document).ready(function(){
         });
         return false; 
     });
-
-    //smooth scroll
     $(window).scroll(function(){
         if ($(this).scrollTop() > 1600){
             $('.pageup').fadeIn();
         } else {
             $('.pageup').fadeOut();
         }
-    }); 
+    });
     $("a[href^='#']").click(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
